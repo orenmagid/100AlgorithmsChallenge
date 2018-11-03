8c7bb1f163bff1a97ec10869cb5c082e460103be
orenmagid <autoimpedicis@gmail.com>
2018-11-03 13:08:33 -0400

    Solved

--- Tester.ts
+++ Tester.ts
@@ -1,15 +1,8 @@
-function add(param1: number, param2: number): number {
-  return param1 + param2;
+function addBorder(picture: string[]): string[] {
+  let newPicture = picture.map(string => "*" + string + "*");
+  newPicture.push("****");
+  newPicture.unshift("****");
+  return newPicture;
 }
 
-function add2(...args: number[]): number {
-  return args.reduce((acc, num) => {
-    return acc + num;
-  });
-}
-
-console.log(add(1, 2));
-console.log(add(3, 2));
-
-console.log(add2(1, 2, 3, 4, 5));
-console.log(add2(2, 3));
+console.log(addBorder(["abc", "ded"]));
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,8 @@
 function addBorder(picture: string[]): string[] {
-
+  let newPicture = picture.map(string => "*" + string + "*");
+  newPicture.push("****");
+  newPicture.unshift("****");
+  return newPicture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
