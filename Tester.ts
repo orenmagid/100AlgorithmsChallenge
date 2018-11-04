8a274c87cacccf2ca70ab040045a0bef0e379194
orenmagid <autoimpedicis@gmail.com>
2018-11-04 17:41:43 -0500

    Solved.

--- Tester.ts
+++ Tester.ts
@@ -3,11 +3,16 @@ function almostIncreasingSequence(sequence: number[]): boolean {
 
   for (let i = 0; i < sequence.length - 1; i++) {
     if (sequence[i] >= sequence[i + 1]) {
+      console.log(sequence[i], sequence[i + 1]);
       counter++;
+      if (sequence[i - 1] >= sequence[i + 1]) {
+        console.log(sequence[i - 1], sequence[i + 1]);
+        return false;
+      }
     }
   }
   return counter < 2;
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1]));
-console.log(almostIncreasingSequence([1, 3, 2]));
+console.log(almostIncreasingSequence([1, 3, 4, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2, 3, 4, 5]));
