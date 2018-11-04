9855b617f5f96ce710baca625df7adf8c18851d7
orenmagid <autoimpedicis@gmail.com>
2018-11-04 17:00:38 -0500

    Solved

--- Tester.ts
+++ Tester.ts
@@ -1,8 +1,22 @@
 function addBorder(picture: string[]): string[] {
+  let wall = "";
+
+  let length =
+    picture[0].length >= picture[picture.length - 1].length
+      ? picture[0].length + 2
+      : picture[picture.length - 1].length + 2;
+
+  for (let i = 0; i < length; i++) {
+    wall += "*";
+  }
+
   let newPicture = picture.map(string => "*" + string + "*");
-  newPicture.push("****");
-  newPicture.unshift("****");
+
+  newPicture.push(wall);
+  newPicture.unshift(wall);
   return newPicture;
 }
 
 console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abcdd", "ded"]));
+console.log(addBorder(["abc", "deddd"]));
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,7 +1,17 @@
 function addBorder(picture: string[]): string[] {
+  let wall = "";
+  let length =
+    picture[0].length > picture[picture.length - 1].length
+      ? picture[0].length
+      : picture[picture.length - 1];
+  for (let i = 0; i < length; i++) {
+    wall.concat("*");
+  }
+
   let newPicture = picture.map(string => "*" + string + "*");
-  newPicture.push("****");
-  newPicture.unshift("****");
+
+  newPicture.push(wall);
+  newPicture.unshift(wall);
   return newPicture;
 }
 
