7ff6c33b95b3e31dc11146c0dff4d3b8ceb69acb
orenmagid <autoimpedicis@gmail.com>
2018-11-04 17:11:34 -0500

    Solved

--- Tester.ts
+++ Tester.ts
@@ -1,6 +1,10 @@
-function addTwoDigits(n) {
-  n = n.toString();
-  return parseInt(n[0]) + parseInt(n[1]);
+function adjacentElementsProduct(inputArray: number[]): number {
+  let product = -Infinity;
+  inputArray.forEach((num, i) => {
+    let temp = num * inputArray[i + 1];
+    if (temp > product) product = temp;
+  });
+  return product;
 }
 
-console.log(addTwoDigits(59));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,10 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+  let product = -Infinity;
+  inputArray.forEach((num, i) => {
+    let temp = num * inputArray[i + 1];
+    if (temp > product) product = temp;
+  });
+  return product;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
