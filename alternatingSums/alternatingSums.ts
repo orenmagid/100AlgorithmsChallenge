daf177a0a90c48f4d62071552f0bd72527828fb9
orenmagid <autoimpedicis@gmail.com>
2018-11-05 15:19:10 -0500

    Solved

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,9 @@
-function alternatingSums(a: number[]): number[] {
-
+function alternatingSums(array: number[]): number[] {
+  let sumArray = [0, 0];
+  for (var i = 0; i < array.length; i++) {
+    i % 2 === 0 ? (sumArray[0] += array[i]) : (sumArray[1] += array[i]);
+  }
+  return sumArray;
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
