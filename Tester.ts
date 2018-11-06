7b452b0474ac7a445b9688b6478b8bd7131f3d12
orenmagid <autoimpedicis@gmail.com>
2018-11-06 10:33:23 -0500

    Solved

--- Tester.ts
+++ Tester.ts
@@ -1,8 +1,28 @@
-function alphabeticShift(inputString: string): string {
-  let array = inputString.split("");
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i].charCodeAt(0));
+function areSimilar(a: number[], b: number[]): boolean {
+  if (a.length !== b.length) return false;
+  let indexes = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      indexes.push(i);
+    }
+  }
+
+  if (indexes.length > 2) {
+    return false;
+  } else {
+    let temp = b[indexes[0]];
+    b[indexes[0]] = b[indexes[1]];
+    b[indexes[1]] = temp;
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
-console.log(alphabeticShift("crazy"));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,5 +1,26 @@
 function areSimilar(a: number[], b: number[]): boolean {
-   
+  if (a.length !== b.length) return false;
+  let indexes = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      indexes.push(i);
+    }
+  }
+
+  if (indexes.length > 2) {
+    return false;
+  } else {
+    let temp = b[indexes[0]];
+    b[indexes[0]] = b[indexes[1]];
+    b[indexes[1]] = temp;
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
