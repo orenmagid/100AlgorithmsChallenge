90695a4b66938b202600932610fd3efefe401e1b
orenmagid <autoimpedicis@gmail.com>
2018-11-06 11:46:05 -0500

    Solved

--- Tester.ts
+++ Tester.ts
@@ -1,13 +1,29 @@
-function arrayChange(arr: number[]): number {
-  let moves = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] <= arr[i - 1]) {
-      moves += arr[i - 1] + 1 - arr[i];
-      arr[i] = arr[i - 1] + 1;
+// Given an array of 2k integers (for some integer k),
+//
+// -   On the 1st, 3rd, 5th, etc. iterations (1-based) replace each pair of consecutive elements with their sum;
+// -   On the 2nd, 4th, 6th, etc. iterations replace each pair of consecutive elements with their product.
+// After the algorithm has finished, there will be a single element left in the array. Return that element.
+
+function arrayConversion(inputArray: number[]): number {
+  let shouldAdd = true;
+  while (inputArray.length > 1) {
+    console.log(inputArray);
+    let tempArray = [];
+    if (shouldAdd) {
+      for (let i = 0; i < inputArray.length; i += 2) {
+        tempArray.push(inputArray[i] + inputArray[i + 1]);
+      }
+      inputArray = tempArray;
+      shouldAdd = !shouldAdd;
+    } else {
+      for (let i = 0; i < inputArray.length; i += 2) {
+        tempArray.push(inputArray[i] * inputArray[i + 1]);
+      }
+      inputArray = tempArray;
+      shouldAdd = !shouldAdd;
     }
   }
-
-  return moves;
+  return inputArray[0];
 }
 
-console.log(arrayChange([1, 1, 1]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,23 @@
 function arrayConversion(inputArray: number[]): number {
-
+  let shouldAdd = true;
+  while (inputArray.length > 1) {
+    console.log(inputArray);
+    let tempArray = [];
+    if (shouldAdd) {
+      for (let i = 0; i < inputArray.length; i += 2) {
+        tempArray.push(inputArray[i] + inputArray[i + 1]);
+      }
+      inputArray = tempArray;
+      shouldAdd = !shouldAdd;
+    } else {
+      for (let i = 0; i < inputArray.length; i += 2) {
+        tempArray.push(inputArray[i] * inputArray[i + 1]);
+      }
+      inputArray = tempArray;
+      shouldAdd = !shouldAdd;
+    }
+  }
+  return inputArray[0];
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
