8eafb89eb1c44b535a25ebb438dbbc846167a626
orenmagid <autoimpedicis@gmail.com>
2018-11-06 11:14:33 -0500

    Solved

--- Tester.ts
+++ Tester.ts
@@ -1,28 +1,13 @@
-function areSimilar(a: number[], b: number[]): boolean {
-  if (a.length !== b.length) return false;
-  let indexes = [];
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      indexes.push(i);
+function arrayChange(arr: number[]): number {
+  let moves = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= arr[i - 1]) {
+      moves += arr[i - 1] + 1 - arr[i];
+      arr[i] = arr[i - 1] + 1;
     }
   }
 
-  if (indexes.length > 2) {
-    return false;
-  } else {
-    let temp = b[indexes[0]];
-    b[indexes[0]] = b[indexes[1]];
-    b[indexes[1]] = temp;
-
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
+  return moves;
 }
 
-console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,13 @@
-function arrayChange(inputArray: number[]): number {
- 
+function arrayChange(arr: number[]): number {
+  let moves = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= arr[i - 1]) {
+      moves += arr[i - 1] + 1 - arr[i];
+      arr[i] = arr[i - 1] + 1;
+    }
+  }
+
+  return moves;
 }
 
 console.log(arrayChange([1, 1, 1]));
